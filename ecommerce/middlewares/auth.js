cab281f712ea99f6de260f4f981feb9269a4066f
Pro-3333 <mahamadouhoudouamadou@gmail.com>
2025-10-04 03:52:54 +0100

    Delete ecommerce directory

--- ecommerce/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-function isAdmin(req, res, next) {
-  if (req.session.user && req.session.user.role === 'admin') {
-    next();
-  } else {
-    res.status(403).send('<h1>Accès refusé</h1><p>Vous n\'avez pas la permission d\'accéder à cette page.</p>');
-  }
-}
-
-module.exports = isAdmin; // export direct
